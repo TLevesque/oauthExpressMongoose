0ed2df888f36e7cd945544392249a705b2bf651a
Thibaut Levesque <levesque.thibaut@gmail.com>
2017-02-24 18:59:15 +0100

    error handling fixed - not blocking anymore

--- server/controllers.js
+++ server/controllers.js
@@ -3,42 +3,57 @@ const Topic = require('./topicsDB');
 const topicController={};
 
 // get all topics :
-topicController.getAll = ( (req, res) => {
+topicController.getAll = ( (req, res, next) => {
   Topic.find({}, function(err, docs) {
-   if(err) return console.error(err);
+   if(err) {
+     console.error(err)
+     return next(err);
+   }
    res.json(docs);
   });
 });
 
 // create new topic
-topicController.createTopic = ( (req, res) => {
+topicController.createTopic = ( (req, res, next) => {
   var obj = new Topic(req.body);
   obj.save(function(err, obj) {
-    if(err) return console.error(err);
+    if(err) {
+      console.error(err)
+      return next(err);
+    }
     res.status(200).json(obj);
   });
 });
 
 // find topic by id
-topicController.findTopic = ( (req, res) => {
+topicController.findTopic = ( (req, res, next) => {
   Topic.findOne({_id: req.params.id}, function(err, obj) {
-    if(err) return console.error(err);
+    if(err) {
+      console.error(err)
+      return next(err);
+    }
     res.json(obj);
   })
 });
 
 // update topic by id
-topicController.updateTopic = ( (req, res) => {
+topicController.updateTopic = ( (req, res, next) => {
   Topic.findOneAndUpdate({_id: req.params.id}, req.body, function(err) {
-    if(err) return console.error(err);
+    if(err) {
+      console.error(err)
+      return next(err);
+    }
     res.sendStatus(200);
   })
 });
 
 // delete topic by id
-topicController.deleteTopic = ( (req, res) => {
+topicController.deleteTopic = ( (req, res, next) => {
   Topic.findOneAndRemove({_id: req.params.id}, function(err) {
-    if(err) return console.error(err);
+    if(err) {
+      console.error(err)
+      return next(err);
+    }
     res.sendStatus(200);
   });
 });
